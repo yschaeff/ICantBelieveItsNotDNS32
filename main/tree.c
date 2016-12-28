50357e6ea37e515ca542dc3ad9dd714f94788019
Yuri Schaeffer <yuri@nlnetlabs.nl>
2016-12-28 12:11:56 +0100

    Change to red black tree.

--- main/tree.c
+++ main/tree.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "esp_log.h"
 
 #include "tree.h"
 
+enum color {RED, BLACK};
+
 struct node {
     void *value;
+    struct node *parent;
     struct node *left;
     struct node *right;
+    enum color color;
 };
 
 struct tree *
@@ -20,30 +25,118 @@ tree_init(int (*cmp)(void *, void *), void (*merge)(void *, void *))
     return tree;
 }
 
+void
+rb_tree_rotright(struct tree *tree, struct node *g)
+{
+    struct node *p = g->left;
+    g->left = p->right;
+    if (p->right) p->right->parent = g;
+    if (!g->parent) {
+        tree->root = p;
+        p->parent = NULL;
+    } else if (g == g->parent->right) {
+        g->parent->right = p;
+        p->parent = g->parent;
+    } else {
+        g->parent->left = p;
+        p->parent = g->parent;
+    }
+    p->right = g;
+    g->parent = p;
+}
+
+void
+rb_tree_rotleft(struct tree *tree, struct node *g)
+{
+    struct node *p = g->right;
+    g->right = p->left;
+    if (p->left) p->left->parent = g;
+    if (!g->parent) {
+        tree->root = p;
+        p->parent = NULL;
+    } else if (g == g->parent->left) {
+        g->parent->left = p;
+        p->parent = g->parent;
+    } else {
+        g->parent->right = p;
+        p->parent = g->parent;
+    }
+    p->left = g;
+    g->parent = p;
+}
+
+void
+rb_tree_fix(struct tree *tree, struct node *node)
+{
+    while (node->parent && node->parent->color == RED) {
+        if (node->parent == node->parent->parent->left) {
+            struct node *uncle = node->parent->parent->right;
+            if (uncle && uncle->color == RED) {
+                node->parent->color = BLACK;
+                uncle->color = BLACK;
+                node->parent->parent->color = RED;
+                node = node->parent->parent;
+            } else {
+                //left right
+                if (node == node->parent->right) {
+                    node = node->parent;
+                    rb_tree_rotleft(tree, node);
+                }
+                //left left
+                node->parent->color = BLACK;
+                node->parent->parent->color = RED;
+                rb_tree_rotright(tree, node->parent->parent);
+            }
+        } else {
+            struct node *uncle = node->parent->parent->left;
+            if (uncle && uncle->color == RED) {
+                node->parent->color = BLACK;
+                uncle->color = BLACK;
+                node->parent->parent->color = RED;
+                node = node->parent->parent;
+            } else {
+                /*right left*/
+                if (node == node->parent->left) {
+                    node = node->parent;
+                    rb_tree_rotright(tree, node);
+                }
+                //right right
+                node->parent->color = BLACK;
+                node->parent->parent->color = RED;
+                rb_tree_rotleft(tree, node->parent->parent);
+            }
+        }
+    }
+    tree->root->color = BLACK;
+}
+
 int
 tree_insert(struct tree *tree, void *value)
 {
-    struct node **parent;
+    struct node **n;
     struct node *node = malloc(sizeof(struct node));
     if (!node) {
         ESP_LOGE(__func__, "Out of memory!");
         return 1;
     }
+    node->parent = NULL;
+    node->color = RED;
     node->value = value;
     node->left = node->right = NULL;
-    parent = &tree->root;
+    n = &tree->root;
 
-    while ( *parent != NULL ) {
-        int c = tree->cmp(node->value, (*parent)->value);
+    while ( *n != NULL ) {
+        int c = tree->cmp(node->value, (*n)->value);
         if (!c) {
-            tree->merge(node->value, (*parent)->value);
+            tree->merge(node->value, (*n)->value);
             free(node);
             return 0;
         }
-        parent = ( c==-1 ) ? &(*parent)->left : &(*parent)->right;
+        node->parent = *n;
+        n = ( c==-1 ) ? &(*n)->left : &(*n)->right;
     }
-    *parent = node;
-    ESP_LOGI(__func__, "insert new");
+    *n = node;
+    rb_tree_fix(tree, node);
     return 0;
 }
 
